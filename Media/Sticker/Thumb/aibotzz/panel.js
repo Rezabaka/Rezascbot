4a5490ba09b1a089518017de45e73f32fa597ce5
Rezabaka <167045812+Rezabaka@users.noreply.github.com>
2024-05-24 03:32:45 +0700

    Add files via upload

---
+++ Media/Sticker/Thumb/aibotzz/panel.js
@@ -0,0 +1,8 @@
+const domain = '' //isi domain mu
+const apikey = '' //apikey plta
+const capikey = '' //apikey pltc
+const eggsnya = '15' 
+const location = '1' 
+
+module.exports = { domain, apikey, capikey, eggsnya, location }
+
